0bc278b92e59f41d9b2d7067297101b8266c678c
lekhnath <lekhnath.yadav1991@gmail.com>
2024-05-20 11:26:00 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import WeatherApp from "./Components/WeatherApp/WeatherApp"
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="App">
+      <WeatherApp />
+    </div>
+    
+    
+    </>  
+  )
+}
+
+export default App
